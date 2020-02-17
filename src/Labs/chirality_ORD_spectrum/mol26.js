b300f98b3b2e28edeb0b5bfaf8c5bf77e62d341d
MayankG20 <mayankgoyal893@gmail.com>
2020-02-17 11:18:48 +0530

    Added molecules with different colour to asymmetric carbon for page 2

---
+++ src/Labs/chirality_ORD_spectrum/mol26.js
@@ -0,0 +1,137 @@
+// var x:=1;
+
+$(function() {
+
+  var x1=1;
+var x2=1;
+  var c1={x:-3.498 ,y: -1.395  ,z:-1.877};
+  var c2={x:-4.783 ,y: -1.579  ,z:-1.029};
+  var c3={x:-2.260 ,y: -1.206  ,z:-0.963};
+  var c4={x:-4.893 ,y: -0.478  ,z: 0.053};
+  var c5={x:-2.472 ,y: -0.107  ,z: 0.127};
+  var c6={x:-1.276 ,y: -0.136  ,z: 1.134};
+  var c7={x:-1.321 ,y:  1.054  ,z: 2.125};
+  var c8={x:-1.471 ,y:  2.397  ,z: 1.375};
+  var c9={x:-2.649 ,y:  2.325  ,z: 0.373};
+  var o1={x:-3.696 ,y: -0.398  ,z: 0.857};
+  var o2={x:-3.634 ,y: -0.293  ,z:-2.774};
+  var o3={x:-2.517 ,y:  1.194  ,z:-0.517};
+  var h1={x:-3.349 ,y: -2.290  ,z:-2.487};
+  var h2={x:-5.667 ,y: -1.555  ,z:-1.675};
+  var h3={x:-4.754 ,y: -2.557  ,z:-0.541};
+  var h4={x:-1.390 ,y: -0.961  ,z:-1.579};
+  var h5={x:-2.058 ,y: -2.162  ,z:-0.470};
+  var h6={x:-5.104 ,y:  0.487  ,z:-0.414};
+  var h7={x:-5.729 ,y: -0.714  ,z: 0.714};
+  var h8={x:-4.360 ,y: -0.549  ,z:-3.383};
+  var h9={x:-1.283 ,y: -1.072  ,z: 1.699};
+  var h01={x:-0.333,y:  -0.083 ,z:  0.582};
+  var h02={x:-2.159,y:   0.927 ,z:  2.816};
+  var h03={x:-0.399,y:   1.069 ,z:  2.715};
+  var h04={x:-1.641,y:   3.207 ,z:  2.090};
+  var h05={x:-0.546,y:   2.614 ,z:  0.831};
+  var h06={x:-3.597,y:   2.276 ,z:  0.915};
+  var h07={x:-2.658,y:   3.235 ,z: -0.228};
+
+  let element1 = $('#cont6');
+  let config1 = { backgroundColor: 'skyblue' };
+  let viewer1 = $3Dmol.createViewer( element1, config1 );
+  viewer1.addLabel("Carbon", {position: {x:-6 , y:3.7, z:5}, backgroundColor: 'skyblue',backgroundOpacity: 0.9,fontSize: 13});
+  viewer1.addLabel("Oxygen", {position: {x:-6 , y:3, z:5}, backgroundColor: 'red',backgroundOpacity: 0.8,fontSize: 13});
+  viewer1.addLabel("Hydrogen", {position: {x:-6 , y:2.3, z:5}, backgroundColor: 'silver',backgroundOpacity: 0.8,fontSize: 13});
+
+  // viewer1.addSphere({center: c1, color: 'skyblue',radius:0.4});
+  viewer1.addSphere({ center: c1, radius: 0.4, color: 'magenta',
+  mid:1.0,   clickable:true,
+  callback:function(){
+      if(x1===1){
+        this.color.setHex(0x0000FF00);
+        x1=0;
+        alert("Correct Choice");
+      }
+      else{
+        this.color.setHex(0XF698FFFF);
+        x1=1;
+      }
+      viewer1.render();
+  }});
+  viewer1.addSphere({center: c2, color: 'skyblue',radius:0.4});
+  viewer1.addSphere({center: c3, color: 'skyblue',radius:0.4});
+  viewer1.addSphere({center: c4, color: 'skyblue',radius:0.4});
+  // viewer1.addSphere({center: c5, color: 'skyblue',radius:0.4});
+  viewer1.addSphere({ center: c5, radius: 0.4, color: 'orange',
+  mid:1.0,   clickable:true,
+  callback:function(){
+      if(x2===1){
+        this.color.setHex(0x0000FF00);
+        x2=0;
+        alert("Correct Choice");
+      }
+      else{
+        this.color.setHex(0XF698FFFF);
+        x2=1;
+      }
+      viewer1.render();
+  }});
+  viewer1.addSphere({center: c6, color: 'skyblue',radius:0.4});
+  viewer1.addSphere({center: c7, color: 'skyblue',radius:0.4});
+  viewer1.addSphere({center: c8, color: 'skyblue',radius:0.4});
+  viewer1.addSphere({center: c9, color: 'skyblue',radius:0.4});
+  
+  viewer1.addSphere({ center: o1, radius: 0.42, color: 'red' });
+  viewer1.addSphere({ center: o2, radius: 0.42, color: 'red' });
+  viewer1.addSphere({ center: o3, radius: 0.42, color: 'red' });
+  
+  viewer1.addSphere({ center: h1, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h2, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h3, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h4, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h5, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h6, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h7, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h8, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h9, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h01, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h02, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h03, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h04, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h05, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h06, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h07, radius: 0.3, color: 'silver' });
+
+  viewer1.addCylinder({ start: c1, end: c2,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c1, end: c3,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c1, end: o2,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c1, end: h1,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c2, end: c4,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c2, end: h2,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c2, end: h3,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c3, end: c5,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c3, end: h4,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c3, end: h5,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c4, end: o1,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c4, end: h6,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c4, end: h7,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: o3, end: c5,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c5, end: c6,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c5, end: o1,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: o3, end: c9,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: o2, end: h8,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c6, end: c7,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c6, end: h9,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c6, end: h01,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c7, end: c8,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c7, end: h03,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c7, end: h02,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c8, end: c9,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c8, end: h04,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c8, end: h05,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start:c9, end: h06,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start:c9, end: h07,radius: .13,color:'white',radiusRadio:0.1,});
+
+  viewer1.zoomTo();
+  viewer1.render();
+  viewer1.setBackgroundColor('black');
+  
+  
+});
---
+++ src/Labs/chirality_ORD_spectrum/mol43.js
@@ -0,0 +1,153 @@
+// var x=1;
+$(function() {
+
+  var x1=1;
+  var x2=1;
+  var c1={x:-1.772 , y:-3.952 , z:-2.282};
+  var c2={x:-0.450 , y:-4.098 , z:-2.930};
+  var c3={x:0.692 , y:-3.337 , z:-2.382};
+  var c4={x:0.265 , y:-1.867 , z:-2.154};
+  var c5={x:-2.114 , y:-2.441 , z:-2.072};
+  var c6={x:-2.773 , y:-0.442 , z:-3.323};
+  var c7={x:-4.020 , y:-0.162 , z:-2.430};
+  var c8={x:-3.824 , y:-0.826 , z:-1.035};
+  var c9={x:-3.370 , y:-2.301 , z:-1.159};
+  var c01={x:-4.311  , y:1.373 , z:-2.351};
+  var c02={x:-5.609  , y:1.755 , z:-1.599};
+  
+  var o1={x:-0.985 , y:-1.787 , z:-1.427};
+  var o2={x:-2.422 , y:-1.845 , z:-3.359};
+  var o3={x:-0.312 , y:-4.812 , z:-3.951};
+
+  var h1={x:-1.720 , y:-4.461 , z:-1.314};
+  var h2={x:-2.549 , y:-4.438 , z:-2.879};
+  var h3={x:0.986 , y:-3.784 , z:-1.427};
+  var h4={x:1.552 , y:-3.375 , z:-3.058};
+  var h5={x:1.032 , y:-1.360 , z:-1.568};
+  var h6={x:0.185 , y:-1.350 , z:-3.113};
+  var h7={x:-2.989 , y:-0.137 , z:-4.350};
+  var h8={x:-1.919  , y:0.148 , z:-2.979};
+  var h9={x:-4.877 , y:-0.642 , z:-2.914};
+  var h01={x:-4.763 , y:-0.807 , z:-0.478};
+  var h02={x:-3.078 , y:-0.269 , z:-0.460};
+  var h03={x:-4.196 , y:-2.881 , z:-1.582};
+  var h04={x:-3.159 , y:-2.696 , z:-0.161};
+  var h05={x:-3.470  , y:1.884 , z:-1.874};
+  var h06={x:-4.398  , y:1.765 , z:-3.369};
+  var h07={x:-5.783  , y:2.830 , z:-1.692};
+  var h08={x:-5.530  , y:1.508 , z:-0.538};
+  var h09={x:-6.464  , y:1.229 , z:-2.024};
+
+  // 2nd Molecule
+  let element1 = $('#cont4');
+  let config1 = { backgroundColor: 'skyblue' };
+  let viewer1 = $3Dmol.createViewer( element1, config1 );
+  viewer1.addLabel("Carbon", {position: {x:0 , y:3.0, z:5}, backgroundColor: 'skyblue',backgroundOpacity: 0.9,fontSize: 13});
+  viewer1.addLabel("Oxygen", {position: {x:0, y:2.3, z:5}, backgroundColor: 'red',backgroundOpacity: 0.8,fontSize: 13});
+  viewer1.addLabel("Hydrogen", {position: {x:0 , y:1.6, z:5}, backgroundColor: 'silver',backgroundOpacity: 0.8,fontSize: 13});
+
+  viewer1.addSphere({center: c1, color: 'skyblue',radius:0.4});
+  viewer1.addSphere({center: c2, color: 'skyblue',radius:0.4});
+  viewer1.addSphere({center: c3, color: 'skyblue',radius:0.4});
+  viewer1.addSphere({center: c4, color: 'skyblue',radius:0.4});
+  // viewer1.addSphere({center: c5, color: 'skyblue',radius:0.4});
+  viewer1.addSphere({ center: c5, radius: 0.4, color: 'skyblue',
+  mid:1.0,   clickable:true,
+  callback:function(){
+      if(x1===1){
+        this.color.setHex(0x0000FF00);
+        x1=0;
+        alert("Correct Choice");
+      }
+      else{
+        this.color.setHex(0XF698FFFF);
+        x1=1;
+      }
+      viewer1.render();
+  }});
+  viewer1.addSphere({center: c6, color: 'skyblue',radius:0.4});
+  // viewer1.addSphere({center: c7, color: 'skyblue',radius:0.4});
+  viewer1.addSphere({ center: c7, radius: 0.4, color: 'skyblue',
+  mid:1.0,   clickable:true,
+  callback:function(){
+      if(x2===1){
+        this.color.setHex(0x0000FF00);
+        x2=0;
+        alert("Correct Choice");
+      }
+      else{
+        this.color.setHex(0XF698FFFF);
+        x2=1;
+      }
+      viewer1.render();
+  }});
+  viewer1.addSphere({center: c8, color: 'skyblue',radius:0.4});
+  viewer1.addSphere({center: c9, color: 'skyblue',radius:0.4});
+  viewer1.addSphere({center: c01, color: 'skyblue',radius:0.4});
+  viewer1.addSphere({center: c02, color: 'skyblue',radius:0.4});
+  
+  viewer1.addSphere({ center: o1, radius: 0.42, color: 'red' });
+  viewer1.addSphere({ center: o2, radius: 0.42, color: 'red' });
+  viewer1.addSphere({ center: o3, radius: 0.42, color: 'red' });
+  
+  viewer1.addSphere({ center: h1, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h2, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h3, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h4, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h5, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h6, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h7, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h8, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h9, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h01, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h02, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h03, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h04, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h05, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h06, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h07, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h08, radius: 0.3, color: 'silver' });
+  viewer1.addSphere({ center: h09, radius: 0.3, color: 'silver' });
+
+  viewer1.addCylinder({ start: c1, end: c2,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c1, end: c5,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c1, end: h2,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c1, end: h1,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: {x:-0.380 , y:-3.95 , z:-2.930}, end: {x:-0.38 , y:-4.6 , z:-3.951},radius: .08,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: {x:-0.380 , y:-4.3 , z:-3.1}, end: {x:-0.38 , y:-4.95 , z:-4.1},radius: .08,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c2, end: c3,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c3, end: c4,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c3, end: h4,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c3, end: h3,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c4, end: o1,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c4, end: h6,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c4, end: h5,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: o1, end: c5,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c5, end: c9,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c5, end: o1,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c5, end: o2,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: o2, end: c6,radius: .13,color:'white',radiusRadio:0.1,});
+  // viewer1.addCylinder({ start: o3, end: c8,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c6, end: c7,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c6, end: h7,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c6, end: h8,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c7, end: c01,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c7, end: c8,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c02, end: h09,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c7, end: h9,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c8, end: c9,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c8, end: h01,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start: c8, end: h02,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start:c9, end: h03,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start:c9, end: h04,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start:c01, end: h05,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start:c01, end: c02,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start:c01, end: h06,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start:c02, end: h07,radius: .13,color:'white',radiusRadio:0.1,});
+  viewer1.addCylinder({ start:c02, end: h08,radius: .13,color:'white',radiusRadio:0.1,});
+  
+  viewer1.zoomTo();
+  viewer1.render();
+  viewer1.setBackgroundColor('black');
+  
+});
